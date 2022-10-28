7186d8f493d9462372a9722ed8300e55bbbd1324
gigi.upNorth <leung.gerry@gmail.com>
2022-10-27 21:39:12 -0400

    [config] move files to the new directory

--- routes/api/index.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const userRoutes = require('./user-routes');
-const workoutRoutes = require('./workout-routes');
-
-router.use('/users', userRoutes);
-router.use('/workouts', workoutRoutes);
-
-module.exports = router;
