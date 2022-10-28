d11c1ae3fba438bfcd3fce94989d0e96758db840
gigi.upNorth <leung.gerry@gmail.com>
2022-10-27 23:16:00 -0400

    [#3 Seeds Mongoose DB] Adding user seeds moving db to config
    Description:
     - Adding faker to generate seeds
     - Move mongoose definition to the config to export mongoose connection

--- server-db/server.js
+++ server-db/server.js
@@ -1,22 +1,31 @@
 const express = require('express');
-const mongoose = require('mongoose');
+const db = require('./config/connection');
 
+// import the db
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+// app.use(express.static('public'));
 
 app.use(require('./routes'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/personal-health-tracker', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+// mongoose.connect(
+// 	process.env.MONGODB_URI ||
+// 		'mongodb://127.0.0.1:27017/personal-health-tracker',
+// 	{
+// 		useNewUrlParser: true,
+// 		useUnifiedTopology: true,
+// 	}
+// );
+
+// // Use this to log mongo queries being executed!
+// mongoose.set('debug', true);
+
+db.once('open', () => {
+	app.listen(PORT, () => {
+		console.log(`🌍 Connected on localhost:${PORT}`);
+	});
 });
-
-// Use this to log mongo queries being executed!
-mongoose.set('debug', true);
-
-app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
