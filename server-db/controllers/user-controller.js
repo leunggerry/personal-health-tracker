d8d7240afbc2846d68f712db36cef7530461331c
gigi.upNorth <leung.gerry@gmail.com>
2022-10-29 13:30:42 -0400

    [#3 User and Workout Getters] User Getters Workout Getters
    Description:
      - Adding getters for users and workouts for all and by ID
      - Adding seeds for Workouts

--- server-db/controllers/user-controller.js
+++ server-db/controllers/user-controller.js
@@ -5,13 +5,7 @@ const userController = {
     // get all users
     getAllUser(req, res) {
         User.find({})
-          .populate({
-            path: 'favWorkouts',
-            select: '-__v'
-          })
-          .select('-__v')
-          .sort({ _id: -1 })
-          .then(dbUserData => res.json(dbUserData))
+          
           .catch(err => {
             console.log(err);
             res.status(400).json(err);
--- server-db/seeders/seeds.js
+++ server-db/seeders/seeds.js
@@ -1,24 +1,43 @@
-const { faker } = require("@faker-js/faker");
+// Data generators
+const { faker } = require('@faker-js/faker');
+const casual = require('casual');
 
-const db = require("../config/connection");
-const { User } = require("../models");
+const db = require('../config/connection');
+const { User, Workout } = require('../models');
 
-db.once("open", async () => {
-  //   await Book.deleteMany({});
-  await User.deleteMany({});
+db.once('open', async () => {
+	await Workout.deleteMany({});
+	await User.deleteMany({});
 
-  // create user data
-  const userData = [];
+	/******************************************************
+	 * !!! User data
+	 ******************************************************/ const userData = [];
 
-  for (let i = 0; i < 50; i += 1) {
-    const username = faker.internet.userName();
-    const email = faker.internet.email(username);
-    const password = faker.internet.password();
+	for (let i = 0; i < 50; i += 1) {
+		const username = faker.internet.userName();
+		const email = faker.internet.email(username);
+		const password = faker.internet.password();
 
-    userData.push({ username, email, password });
-  }
-  const createdUsers = await User.collection.insertMany(userData);
+		userData.push({ username, email, password });
+	}
+	const createdUsers = await User.collection.insertMany(userData);
 
-  console.log("all done!");
-  process.exit(0);
+	/******************************************************
+	 * !!! Workout data
+	 ******************************************************/
+
+	// Create workout data
+	const workoutData = [];
+	for (let i = 0; i < 50; i += 1) {
+		const workoutName = casual.title;
+		const workoutDescription = casual.short_description;
+		const setsCount = Math.floor(casual.random * 6);
+		const repsCount = Math.floor(casual.random * 10);
+
+		workoutData.push({ workoutName, workoutDescription, setsCount, repsCount });
+	}
+
+	const createdWorkouts = await Workout.collection.insertMany(workoutData);
+	console.log('all done!');
+	process.exit(0);
 });
