78bf523dbc2289138ffec2efcd993bf5ec7da119
gigi.upNorth <leung.gerry@gmail.com>
2022-10-29 13:30:42 -0400

    [#3 User and Workout Getters] User Getters Workout Getters
    Description:
      - Adding getters for users and workouts for all and by ID
      - Adding seeds for Workouts

--- server-db/schemas/resolvers.js
+++ server-db/schemas/resolvers.js
@@ -1,7 +1,49 @@
+const { User, Workout } = require('../models');
+
 const resolvers = {
 	Query: {
-		helloWorld: () => {
-			return 'Hello world!';
+		/******************************************************
+		 * !!! User Queries
+		 ******************************************************/
+		/**
+		 *
+		 * @returns
+		 */
+		users: async () => {
+			return User.find({})
+				.populate({
+					path: 'favWorkouts',
+					select: '-__v',
+				})
+				.select('-__v')
+				.sort({ _id: -1 });
+		},
+
+		//get a user by ID
+		user: async (parent, { id }) => {
+			// setup params to include ID outhers return all users
+			return User.findById(id)
+				.populate({
+					path: 'favWorkouts',
+					select: '-__v',
+				})
+				.select('-__v')
+				.sort({ _id: -1 });
+		},
+
+		/******************************************************
+		 * !!! Workout Queries
+		 ******************************************************/
+		//get all workouts
+		workouts: async () => {
+			return Workout.find({}).sort({ _id: -1 });
+		},
+
+		getWorkoutById: async (parent, { id }) => {
+			//console.log(workoutName);
+			const params = id ? { id } : {};
+			console.log(params);
+			return Workout.findById(id);
 		},
 	},
 };
--- server-db/schemas/typeDefs.js
+++ server-db/schemas/typeDefs.js
@@ -3,8 +3,31 @@ const { gql } = require('apollo-server-express');
 
 // create our TypeDefs
 const typeDefs = gql`
+	type User {
+		_id: ID
+		username: String
+		favWorkouts: [Workout]
+		mondayWorkouts: [Workout]
+		wednesdayWorkouts: [Workout]
+		thursdayWorkouts: [Workout]
+		fridayWorkouts: [Workout]
+		saturdayWorkouts: [Workout]
+		sundayWorkouts: [Workout]
+	}
+
+	type Workout {
+		_id: ID
+		workoutName: String
+		workoutDescription: String
+		setsCount: Int
+		repsCount: Int
+	}
+
 	type Query {
-		helloWorld: String
+		users: [User]
+		user(id: String): User
+		workouts: [Workout]
+		getWorkoutById(id: String): Workout
 	}
 `;
 
