768dea988ba392250d4a0e58e8049b45385b81ab
Joe <105400893+joesen-dev@users.noreply.github.com>
2022-10-29 20:20:39 -0400

    [Install React scheduler #16] - Installed dependencies and rendered the calendar to the Dashboard.  - NOTE: when running nodemon, i'm getting several errors from the node_modules after installing all dependencies.

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -3,15 +3,50 @@
  *  - Favories (favorited workouts)
  *  - Calendar
  */
-import React from 'react';
-import FavoriteWorkouts from '../components/FavoriteWorkouts';
+import * as React from 'react';
+import Paper from '@mui/material/Paper';
+import { ViewState } from '@devexpress/dx-react-scheduler';
+import {
+	Scheduler,
+	MonthView,
+	Appointments,
+} from '@devexpress/dx-react-scheduler-material-ui';
+import moment from 'moment';
+
+// import { appointments } from '../../../demo-data/month-appointments';
+
+// import FavoriteWorkouts from '../components/FavoriteWorkouts';
 
 const Dashboard = () => {
+	const currentDate = moment();
+	// const currentDate = '2018-07-17';
+
+	// This is a placeholder
+	const schedulerData = [
+		{
+			startDate: currentDate,
+			endDate: '2022-11-01T11:00',
+			title: 'Meeting',
+		},
+		{
+			startDate: currentDate,
+			endDate: '2022-11-01T13:30',
+			title: 'Go to a gym',
+		},
+	];
+
 	return (
 		<div className="container">
 			<h2>Dashboard page</h2>
 			<div className="container my-1">
-				<FavoriteWorkouts />
+				{/* <FavoriteWorkouts /> */}
+				<Paper>
+					<Scheduler data={schedulerData}>
+						<ViewState currentDate={currentDate} />
+						<MonthView />
+						<Appointments />
+					</Scheduler>
+				</Paper>
 			</div>
 		</div>
 	);
