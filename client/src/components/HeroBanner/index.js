33d1de832a05039c006bd790239f376d38db768f
bebo <ibrahimimran96@gmail.com>
2022-11-02 18:09:13 -0400

    Added fonts using Chakra UI. Changed index.css for Dashboard/enhanced responsiveness

--- client/src/components/HeroBanner/index.js
+++ client/src/components/HeroBanner/index.js
@@ -1,22 +1,43 @@
 import React from 'react';
 import placeHolderImage from '../../assets/images/kelly-sikkema-bE6k8SQT2FQ-unsplash.jpg';
+import {
+	ChakraProvider,
+	extendTheme,
+	Heading,
+	Text,
+  } from "@chakra-ui/react"
+  import Fonts  from "../../assets/Fonts";
 
-function HeroBanner() {
+  const theme = extendTheme({
+	fonts: {
+	  heading: "Open Sans",
+	  body: "Verdana",
+	},
+  })
+  function HeroBanner() {
 	return (
+		<ChakraProvider theme={theme}>
+		<Fonts />
+		
+		  {/* <Stack> */}
 		<section className="hero">
 			<div className="hero-cta">
-				<h2>Make your health a priority.</h2>
-				<p>
+			<Heading fontSize='6xl'>Make your health a priority.</Heading>
+			<br />
+			<Text fontSize='2xl'>
 					Add Capstone Fitness to your workout routine and see the results for
 					yourself! This daily workout tracker will help you maintain your
 					physical health by adding workouts to your calendar and tracking your
 					progress over the months. So start your journey today!
-				</p>
+			</Text>
 			</div>
 			<div className="hero-form">
 				<img src={placeHolderImage} className="img-thumbnail" alt="..." />
 			</div>
 		</section>
+			
+		  {/* </Stack> */}
+	  </ChakraProvider>
 	);
 }
 
