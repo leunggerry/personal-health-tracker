de13688472013ab125e0c19747ce92caa97230d8
Joe <105400893+joesen-dev@users.noreply.github.com>
2022-11-02 12:01:32 -0400

    Removing un-used files

--- client/src/App.js
+++ client/src/App.js
@@ -10,15 +10,12 @@ import { setContext } from '@apollo/client/link/context';
 import { Box } from '@chakra-ui/react';
 
 import LandingPage from './pages/LandingPage';
-import Detail from './pages/Detail';
 import NoMatch from './pages/NoMatch';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Navigation from './components/Navigation';
 import Footer from './components/Footer';
 import { StoreProvider } from './utils/GlobalState';
-import Success from './pages/Success';
-import OrderHistory from './pages/OrderHistory';
 import Profile from './pages/Profile';
 import Workouts from './pages/Workouts';
 import Dashboard from './pages/Dashboard';
@@ -42,45 +39,40 @@ const client = new ApolloClient({
 	cache: new InMemoryCache(),
 });
 
-
-
 function App() {
 	const [loading, setLoading] = useState(false);
 
-useEffect(() => {
-  setLoading(true);
-  setTimeout(() => {
-	setLoading(false);
-  }, 2000);
-}, []);
+	useEffect(() => {
+		setLoading(true);
+		setTimeout(() => {
+			setLoading(false);
+		}, 2000);
+	}, []);
 	return (
-
 		<ApolloProvider client={client}>
 			<Router>
-			    	{loading ? (
-        			<div className="loader-container">
-      	 			 <div className="spinner"></div>
-       					 </div>
-     						) : (
-				<StoreProvider>
-					<Box>
-					<Navigation />
-					<Routes>
-						<Route path="/" element={<LandingPage />} />
-						<Route path="/login" element={<Login />} />
-						<Route path="/signup" element={<Signup />} />
-						<Route path="/success" element={<Success />} />
-						<Route path="/orderHistory" element={<OrderHistory />} />
-						<Route path="/profile" element={<Profile />} />
-						<Route path="/workouts" element={<Workouts />} />
-						<Route path="/dashboard" element={<Dashboard />} />
-						<Route path="/products/:id" element={<Detail />} />
-						<Route path="*" element={<NoMatch />} />
-					</Routes>
-					<Footer />
-					</Box>
-				</StoreProvider>
-			)}
+				{loading ? (
+					<div className="loader-container">
+						<div className="spinner"></div>
+					</div>
+				) : (
+					<StoreProvider>
+						<Box>
+							<Navigation />
+							<Routes>
+								<Route path="/" element={<LandingPage />} />
+								<Route path="/login" element={<Login />} />
+								<Route path="/signup" element={<Signup />} />
+								<Route path="/profile" element={<Profile />} />
+								<Route path="/workouts" element={<Workouts />} />
+								<Route path="/dashboard" element={<Dashboard />} />
+								{/* TODO: Can we take this out. I don't think we are using it */}
+								<Route path="*" element={<NoMatch />} />
+							</Routes>
+							<Footer />
+						</Box>
+					</StoreProvider>
+				)}
 			</Router>
 		</ApolloProvider>
 	);
