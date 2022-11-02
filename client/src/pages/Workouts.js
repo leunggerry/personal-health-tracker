affe9d4bc0891b901f2f07a43c4b5724cd969a8f
Joe <105400893+joesen-dev@users.noreply.github.com>
2022-11-01 20:22:57 -0400

    Workoutpage and updated package.json

--- client/src/pages/Workouts.js
+++ client/src/pages/Workouts.js
@@ -3,9 +3,12 @@
  *  - Workout cards
  */
 import React, { useState, useEffect } from 'react';
+import { Container, CardColumns, Card, Button } from 'react-bootstrap';
 
-import { useMutation } from '@apollo/client';
+import { useMutation, useQuery } from '@apollo/client';
+import { useStoreContext } from '../utils/GlobalState';
 import WorkoutList from '../components/WorkoutList';
+import { QUERY_ALL_WORKOUTS } from '../utils/queries';
 import { ADD_FAV_WORKOUT } from '../utils/mutations';
 
 import { saveWorkoutIds, getWorkoutIds } from '../utils/localStorage';
@@ -13,19 +16,24 @@ import { saveWorkoutIds, getWorkoutIds } from '../utils/localStorage';
 const Workouts = () => {
 	const [addWorkout] = useMutation(ADD_FAV_WORKOUT);
 
+	// useQuery to get all workouts
+	const { loading, data } = useQuery(QUERY_ALL_WORKOUTS);
+	const dbWorkouts = data?.workouts || {};
+	console.log('DB Workouts: ', dbWorkouts.workouts);
+
 	// state to hold workout data
 	const [loadedWorkouts, setloadedWorkouts] = useState([]);
 
 	// state to hold saved workoutId values
-	const [saveWorkoutIds, setsaveWorkoutIds] = useState('Hello World');
+	const [savedWorkout, setsaveWorkoutIds] = useState(dbWorkouts);
 
 	// save list workoutIds to localStorage
 	useEffect(() => {
-		return () => saveWorkoutIds(saveWorkoutIds);
+		return () => saveWorkoutIds(savedWorkout);
 	});
 
 	// handle adding a workout
-	const handleSaveBook = async (workoutId) => {
+	const handleAddWorkout = async (workoutId) => {
 		// find the workout by it's id
 		const workoutToSave = loadedWorkouts.find(
 			(workout) => workout.id === workoutId
@@ -41,10 +49,36 @@ const Workouts = () => {
 			console.error(err);
 		}
 	};
+
+	if (loading) {
+		return <h2>LOADING...</h2>;
+	}
 	return (
-		<div className="container">
-			<WorkoutList />
-		</div>
+		<>
+			<Container>
+				<CardColumns>
+					{dbWorkouts.map((workoutItem) => {
+						return (
+							<Card key={workoutItem.Id} border="dark">
+								<Card.Body>
+									<Card.Title>{workoutItem.workoutName}</Card.Title>
+									<p className="small"></p>
+									<Card.Text>
+										Description: {workoutItem.workoutDescription}
+									</Card.Text>
+									<Button
+										className="btn-block btn-danger"
+										onClick={() => handleAddWorkout(workoutItem.Id)}
+									>
+										+ Add
+									</Button>
+								</Card.Body>
+							</Card>
+						);
+					})}
+				</CardColumns>
+			</Container>
+		</>
 	);
 };
 
--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -6,12 +6,12 @@ const { Provider } = StoreContext;
 
 const StoreProvider = ({ value = [], ...props }) => {
 	const [state, dispatch] = useProductReducer({
-		products: [],
-		cart: [],
-		cartOpen: false,
-		categories: [],
-		currentCategory: '',
+		favoriteWorkouts: [],
+		workouts: [],
+		currentFavorites: '',
 	});
+
+	console.log('GLOBAL-STATE: ', state);
 
 	return <Provider value={[state, dispatch]} {...props} />;
 };
@@ -21,3 +21,28 @@ const useStoreContext = () => {
 };
 
 export { StoreProvider, useStoreContext };
+// =========================================================
+
+// import React, { createContext, useContext } from 'react';
+// import { useProductReducer } from './reducers';
+
+// const StoreContext = createContext();
+// const { Provider } = StoreContext;
+
+// const StoreProvider = ({ value = [], ...props }) => {
+// 	const [state, dispatch] = useProductReducer({
+// 		products: [],
+// 		cart: [],
+// 		cartOpen: false,
+// 		categories: [],
+// 		currentCategory: '',
+// 	});
+
+// 	return <Provider value={[state, dispatch]} {...props} />;
+// };
+
+// const useStoreContext = () => {
+// 	return useContext(StoreContext);
+// };
+
+// export { StoreProvider, useStoreContext };
