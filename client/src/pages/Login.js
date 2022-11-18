2a59ac13e614814ff93473fdb6e8d5460505cf8f
Joe <105400893+joesen-dev@users.noreply.github.com>
2022-11-18 01:35:03 -0500

    refactored Login Page with Tailwing

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Label, TextInput, Button, Checkbox } from 'flowbite-react';
 // import { Container, Row, Col } from 'react-bootstrap';
 import { Container, Grid, SimpleGrid } from '@chakra-ui/react';
 
@@ -37,45 +38,32 @@ function Login(props) {
 	};
 
 	return (
-		<Container fluid="md" className="main" centerContent>
-			<Grid>
-				<Link to="/signup" style={{ padding: '40px' }}>
-					← Go to Signup
-				</Link>
-				<SimpleGrid w={[300, 400, 500]} minChildWidth="120px">
-					<div className="login-card">
-						<h2>Login</h2>
-						<h3>Enter you credentials</h3>
-						<form className="login-form" onSubmit={handleFormSubmit}>
-							<input
-								placeholder="Username"
-								name="username"
-								type="username"
-								id="username"
-								onChange={handleChange}
-							/>
-							<input
-								placeholder="******"
-								name="password"
-								type="password"
-								id="pwd"
-								onChange={handleChange}
-							/>
-							{error ? (
-								<div>
-									<p className="error-text">
-										The provided credentials are incorrect
-									</p>
-								</div>
-							) : null}
-							{/* TODO: make sure the forgor your password link leads somewhere */}
-							<Link to="/signup">Forgot your password?</Link>
-							<button type="submit">LOGIN</button>
-						</form>
+		<div class="container mx-auto xs:flex xs:justify-center md:flex">
+			<form className="flex flex-col gap-4 rounded-lg bg-slate-300 pt-16 px-8 pb-11 xs:w-72 sm:w-80 2xl:w-96 ">
+				<div>
+					<div className="mb-2 block">
+						<Label htmlFor="email1" value="Your email" />
 					</div>
-				</SimpleGrid>
-			</Grid>
-		</Container>
+					<TextInput
+						id="email1"
+						type="email"
+						placeholder="name@flowbite.com"
+						required={true}
+					/>
+				</div>
+				<div>
+					<div className="mb-2 block">
+						<Label htmlFor="password1" value="Your password" />
+					</div>
+					<TextInput id="password1" type="password" required={true} />
+				</div>
+				<div className="flex items-center gap-2">
+					<Checkbox id="remember" />
+					<Label htmlFor="remember">Remember me</Label>
+				</div>
+				<Button type="submit">Submit</Button>
+			</form>
+		</div>
 	);
 }
 
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
 	content: [
 		'./src/**/*.{js,jsx,ts,tsx}',
 		'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
 	],
 	theme: {
+		screens: {
+			xs: '375px',
+			...defaultTheme.screens,
+		},
 		extend: {
 			fontFamily: {
 				'Open Sans': ['Open Sans', 'sans-serif'],
