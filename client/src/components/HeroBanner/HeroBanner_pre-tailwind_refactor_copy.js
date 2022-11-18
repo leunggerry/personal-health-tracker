09d4d10d69bf5ea6c938c8e260145dfbefcd70fa
Joe <105400893+joesen-dev@users.noreply.github.com>
2022-11-17 22:35:38 -0500

    Refactoring HeroBanner with Tailwind

--- client/src/components/HeroBanner/HeroBanner_pre-tailwind_refactor_copy.js
+++ client/src/components/HeroBanner/HeroBanner_pre-tailwind_refactor_copy.js
@@ -11,30 +11,28 @@ const theme = extendTheme({
 });
 function HeroBanner() {
 	return (
-		<section className="container mx-auto">
-			<div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
-				{/* CTA */}
-				<div className="basis-1/3 flex-1">
-					<h1 className="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
-						Make your{' '}
-						<span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
-							health a priority
-						</span>{' '}
-					</h1>
+		<ChakraProvider theme={theme}>
+			<Fonts />
+
+			{/* <Stack> */}
+			<section className="hero">
+				<div className="hero-cta">
+					<Heading fontSize="6xl">Make your health a priority.</Heading>
 					<br />
-					<p class="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">
+					<Text fontSize="2xl">
 						Add Capstone Fitness to your workout routine and see the results for
 						yourself! This daily workout tracker will help you maintain your
 						physical health by adding workouts to your calendar and tracking
 						your progress over the months. So start your journey today!
-					</p>
+					</Text>
 				</div>
-				{/* Image div */}
-				<div className="basis-1/3 flex-1">
+				<div className="hero-form">
 					<img src={placeHolderImage} className="img-thumbnail" alt="..." />
 				</div>
-			</div>
-		</section>
+			</section>
+
+			{/* </Stack> */}
+		</ChakraProvider>
 	);
 }
 
--- client/src/components/Navigation/index.js
+++ client/src/components/Navigation/index.js
@@ -136,19 +136,23 @@ function Navigation() {
 						>
 							<ul className="flex flex-col p-4 mt-4 border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
 								<li>
-									<Link
-										to="/signup"
-										className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-									>
-										Signup
+									<Link to="/signup">
+										<button
+											type="button"
+											className="text-white bg-gray-600 hover:bg-gray-900 font-medium rounded-lg text-sm px-4 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+										>
+											Signup
+										</button>
 									</Link>
 								</li>
 								<li>
-									<Link
-										to="/login"
-										className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-									>
-										Login
+									<Link to="/login">
+										<button
+											type="button"
+											className="text-white bg-gray-600 hover:bg-gray-900 font-medium rounded-lg text-sm px-4 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+										>
+											Login
+										</button>
 									</Link>
 								</li>
 							</ul>
